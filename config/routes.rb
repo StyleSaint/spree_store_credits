dea6522815158870ba4bad9729a73790fdd16402
shenwei235 <shenwei235@gmail.com>
2013-12-11 12:49:10 +0800

    Update routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Spree::Core::Engine.routes.append do
   namespace :admin do
-    resources :store_credits
+    resources :store_credits do
+      get :history, :on => :member
+    end
     resources :users do
       resources :store_credits
     end
