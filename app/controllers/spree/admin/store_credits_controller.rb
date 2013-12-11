81cfbb995efba9139f8b201bebae4edaa094d98f
shenwei235 <shenwei235@gmail.com>
2013-12-11 12:46:22 +0800

    Re #945 add user store credits history page

--- app/controllers/spree/admin/store_credits_controller.rb
+++ app/controllers/spree/admin/store_credits_controller.rb
@@ -3,6 +3,12 @@ module Spree
     before_filter :check_amounts, :only => [:edit, :update]
     prepend_before_filter :set_remaining_amount, :only => [:create, :update]
 
+    def history
+      return if params[:id].nil?
+      @user = Spree::User.find(params[:id])
+      @user_store_credits = @user.store_credits if @user.has_store_credit?
+    end
+
     private
     def check_amounts
       if (@store_credit.remaining_amount < @store_credit.amount)
